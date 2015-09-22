5a0097c9f16359f4ecabda0bcb94bcb0a43b681e
Samuel Ballan <sgb4622@gmail.com>
2015-09-22 18:10:40 -0400

    HTML Template Magic

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
+var people =  {
+    title: 'homepage',
+    people: {
+      jane:{
+        name: "Jane Doe"
+      },
+      tom:{
+        name: "Tom J"
+      },
+      chris:{
+        name: "Chris Q"
+      }
+    }
+  };
 
 var app = express();
 var router = express.Router();
 
+swig.renderFile(__dirname + '/views/index.html', people, function(err, output) {
+  console.log(output);
+});
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
+
+router.get('/', function (req, res) {
+  res.render('index', people);
+});
+
+
+
+
 app.use(morgan('tiny'));
 
-router.get('/', function(req, res) {
-  res.send("Homepage");
-});
+// router.get('/', function(req, res) {
+//   res.send("Homepage");
+// });
 
 router.get('/news', function(req, res) {
   res.send("News Page");
