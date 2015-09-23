bf2c050d3900ec65836d55e1d59a39af3fd33100
Samuel Ballan <sgb4622@gmail.com>
2015-09-23 15:29:36 -0400

    Exercise in static middleware w/o using built in Express capabilities. For learning purposes.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express');
+var app = express();
+var fs = require('fs');
 var morgan = require('morgan');
+var mime = require('mime');
+
 var swig = require('swig');
 var routes = require('./routes');
 var bodyParser = require('body-parser');
 var socketio = require('socket.io');
 
+//static file middleware
+app.use(function(req, res, next) {
+  var mimeType = mime.lookup(req.path);
+  fs.readFile('./public/' + req.path, function(err, fileBuffer) {
+    if(err) return next();
+    res.header('Content-Type', mimeType);
+    res.send(fileBuffer);
+  });
+});
 
 
-var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
